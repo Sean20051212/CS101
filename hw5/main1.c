bac06e4a63bd303f5c3b4522d7a917d11d11db39
Sean20051212 <105063897+Sean20051212@users.noreply.github.com>
2024-12-10 11:46:12 +0800

    Update

---
+++ hw5/main1.c
@@ -0,0 +1,20 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int n = 7;
+    int s = n;
+    for (int i = 1; i <= n ;i++) {
+        
+        for (int k = 1; k <= n-i ;k++) {
+            printf(" ");
+        }
+        for (int g = 1; g <= i ;g++) {
+            printf("%d ",i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
